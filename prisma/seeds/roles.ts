4bbeb198fa46d3614b60728bb422cd1221eda9b1
hebertsanto <hebertsantos0704@gmail.com>
2024-06-05 17:55:54 -0300

    fix : ajust correct roles

--- prisma/seeds/roles.ts
+++ prisma/seeds/roles.ts
@@ -7,21 +7,31 @@ export async function seedRoles() {
   await prisma.roles.createMany({
     data: [
       {
-        id: '665fdcce-8795-4cec-8c91-0ff1c47e5284',
-        name: 'Admin',
-        description: 'Responsible for managing a resource'
+        id: "665fdcce-8795-4cec-8c91-0ff1c47e5284",
+        name: "Admin",
+        description: "Responsible for managing a resource",
       },
       {
-        id: 'faf787c0-6b97-4dae-84e4-ca947c790bf2',
-        name: 'Train Operator',
-        description: 'Responsible for operating the train'
+        id: "faf787c0-6b97-4dae-84e4-ca947c790bf2",
+        name: "Train Operator",
+        description: "Responsible for operating the train",
       },
       {
-        id: 'b7df9f50-0b5c-45f9-ae54-5c37de8c97ae',
-        name: 'Ticket Booth Attendant',
-        description: 'Responsible for attending the box office'
-      }
-    ]
+        id: "b7df9f50-0b5c-45f9-ae54-5c37de8c97ae",
+        name: "Ticket Booth Attendant",
+        description: "Responsible for attending the box office",
+      },
+      {
+        id: "477b3243-8a2e-484f-baf2-18bf7f08aa31",
+        name: "Maintains technical services",
+        description: "Responsible for attending technical services",
+      },
+      {
+        id: "8659bc51-fbef-42e7-a0c5-39fe219d2e27",
+        name: "Passenger",
+        description: "Passanger role",
+      },
+    ],
   });
 
   console.log("Roles seeded successfully");
