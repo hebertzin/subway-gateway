6ea59de51cda2d34b747cc9a79c8ea40bcb873e8
hebertsanto <hebertsantos0704@gmail.com>
2024-08-22 10:22:17 -0300

    chore : add logger to the project

---
+++ src/domain/logger.ts
@@ -0,0 +1,7 @@
+export interface ILogger {
+  info(message: string): void;
+  error(message: string): void;
+  warn(message: string): void;
+  debug(message: string): void;
+  log(level: string, message: string): void;
+}
