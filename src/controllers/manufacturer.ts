2bbeb57c9d2cb0bb8964ccb751129b9769f64234
hebertsanto <hebertsantos0704@gmail.com>
2024-08-22 10:31:54 -0300

    feat : add zod validations in manufacturer

--- src/controllers/manufacturer.ts
+++ src/controllers/manufacturer.ts
@@ -1,15 +1,30 @@
 import { Request } from "express";
+import { z } from "zod";
 import { Controller, HttpResponse } from "../domain/controller";
 import { HttpStatusCode } from "../domain/http";
 import { ManufacturerCreateInput } from "../repositories/manufacturer";
 import { IManufacturerService } from "../services/manufacturer";
+
+const manufacturerSchemaValidation = z.object({
+  name: z.string().min(1, "Name is required"),
+  phone: z.string().min(9, "Phone number must be at least 10 digits"),
+  email: z.string().email("Invalid email address"),
+  street: z.string().min(1, "Street is required"),
+  number: z.string().min(1, "Number is required"),
+  city: z.string().min(1, "City is required"),
+  state: z.string().length(2, "State must be a 2-letter code"),
+  zip_code: z.string().min(1, "zip code is required"),
+  website: z.string().url("Invalid website URL"),
+});
 
 export class ManufacturerController implements Controller {
   constructor(readonly manufacturerService: IManufacturerService) {}
 
   async handle(request: Request): Promise<HttpResponse> {
     try {
-      const data = request.body as ManufacturerCreateInput;
+      const data = manufacturerSchemaValidation.parse(
+        request.body
+      ) as ManufacturerCreateInput;
       const manufacturer = await this.manufacturerService.invoke(data);
       return {
         msg: "Manufacturer created successfully",
@@ -20,7 +35,6 @@ export class ManufacturerController implements Controller {
       return {
         msg: error.message,
         statusCode: error.code,
-        body: error,
       };
     }
   }
