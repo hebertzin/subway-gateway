f676e4836fef0b9a352134393a11feb68ef23969
hebertsanto <hebertsantos0704@gmail.com>
2024-08-25 10:56:04 -0300

    refactor : add interface as return type in use case

--- src/infra/factory/usecases/add-manufacturer-factory.ts
+++ src/infra/factory/usecases/add-manufacturer-factory.ts
@@ -1,8 +1,8 @@
-import { AddManufacturerUseCase } from "../../../application/usecases/manufacturer";
+import { AddManufacturerUseCase, IAddManufacturerUseCase } from "../../../application/usecases/manufacturer";
 import { LoggerService } from "../../logging/logger";
 import { ManufacturerRepository } from "../../repositories/manufacturer";
 
-export const makeDbAddManufacturer = (): AddManufacturerUseCase => {
+export const makeDbAddManufacturer = (): IAddManufacturerUseCase => {
   const manufacturerRepository = new ManufacturerRepository();
   const loggerService = new LoggerService();
   return new AddManufacturerUseCase(manufacturerRepository, loggerService);
