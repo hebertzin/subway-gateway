f7f3e117391cc33ade13d6b8d23955c15b71f321
hebertsanto <hebertsantos0704@gmail.com>
2024-08-31 10:49:12 -0300

    feat : creating the file structure to add tests

--- src/infra/repositories/manufacturer.ts
+++ src/infra/repositories/manufacturer.ts
@@ -1,12 +1,12 @@
-import { Manufacturer, ManufacturerCreateInput } from "../../domain/manufacturer";
+import { CreateManufacturerInput, Manufacturer } from "../../domain/manufacturer";
 import { prisma } from "../database/prisma";
 
 export interface IManufacturerRepository {
-  create(data: ManufacturerCreateInput): Promise<Manufacturer>;
+  create(data: CreateManufacturerInput): Promise<Manufacturer>;
 }
 
 export class ManufacturerRepository implements IManufacturerRepository {
-  async create(data: ManufacturerCreateInput): Promise<Manufacturer> {
+  async create(data: CreateManufacturerInput): Promise<Manufacturer> {
     const manufacturer = await prisma.manufacturer.create({ data });
     return { manufacturer };
   }
