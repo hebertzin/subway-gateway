913ab1c834b9be0b58b1d5959f5e975b67dc3f18
hebertsanto <hebertsantos0704@gmail.com>
2024-09-23 10:42:10 -0300

    feat : add more validations

--- src/presentation/validations/manufacturer.ts
+++ src/presentation/validations/manufacturer.ts
@@ -1,13 +1,20 @@
 import z from "zod";
 
 export const manufacturerSchemaValidation = z.object({
-  name: z.string().min(1, "Name is required"),
-  phone: z.string().min(9, "Phone number must be at least 10 digits"),
-  email: z.string().email("Invalid email address"),
+  name: z
+    .string({ message: "Name must be a string" })
+    .min(1, "Name is required"),
+  phone: z
+    .string({ message: "Pone must be a string" })
+    .min(9, "Phone number must be at least 10 digits"),
+  email: z
+    .string()
+    .email("Invalid email address")
+    .min(1, { message: "Email is required" }),
   street: z.string().min(1, "Street is required"),
   number: z.string().min(1, "Number is required"),
   city: z.string().min(1, "City is required"),
   state: z.string().length(2, "State must be a 2-letter code"),
   zip_code: z.string().min(1, "zip code is required"),
-  website: z.string().url("Invalid website URL"),
+  website: z.string().url("Invalid website URL").min(1, { message: "Url is required"}),
 });
