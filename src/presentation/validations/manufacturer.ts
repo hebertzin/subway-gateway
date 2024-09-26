9c7510f156d110067e233e97df777beff22595ce
hebertsanto <hebertsantos0704@gmail.com>
2024-09-25 22:49:45 -0300

    feat : add more validations to the manufacturer schema

--- src/presentation/validations/manufacturer.ts
+++ src/presentation/validations/manufacturer.ts
@@ -1,20 +1,33 @@
-import z from "zod";
+import { z } from "zod";
+import { brazilianPhoneRegex, brazilianZipCodeRegex } from "../utils/regex";
 
 export const manufacturerSchemaValidation = z.object({
   name: z
     .string({ message: "Name must be a string" })
     .min(1, "Name is required"),
   phone: z
-    .string({ message: "Pone must be a string" })
-    .min(9, "Phone number must be at least 10 digits"),
+    .string({ message: "Phone must be a string" })
+    .min(10, { message: "Phone number must be at least 10 digits" })
+    .regex(brazilianPhoneRegex, {
+      message:
+        "Phone number must be in the format (XX) XXXXX-XXXX or (XX) XXXX-XXXX",
+    }),
   email: z
     .string()
-    .email("Invalid email address")
+    .email({ message: "Invalid email address" })
     .min(1, { message: "Email is required" }),
-  street: z.string().min(1, "Street is required"),
-  number: z.string().min(1, "Number is required"),
-  city: z.string().min(1, "City is required"),
-  state: z.string().length(2, "State must be a 2-letter code"),
-  zip_code: z.string().min(1, "zip code is required"),
-  website: z.string().url("Invalid website URL").min(1, { message: "Url is required"}),
+  street: z.string().min(1, { message: "Street is required" }),
+  number: z.string().min(1, { message: "Number is required" }),
+  city: z.string().min(1, { message: "City is required" }),
+  state: z.string().length(2, { message: "State must be a 2-letter code" }),
+  zip_code: z
+    .string()
+    .min(8, { message: "Zip code must be at least 8 characters" })
+    .regex(brazilianZipCodeRegex, {
+      message: "Zip code must be in the format XXXXX-XXX",
+    }),
+  website: z
+    .string()
+    .url({ message: "Must be a valid URL" })
+    .min(1, { message: "URL is required" }),
 });
