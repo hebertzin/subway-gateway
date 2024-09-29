8e7e2de0328223c87bb42cfa7622d101c9edb074
hebertsanto <hebertsantos0704@gmail.com>
2024-09-29 12:33:17 -0300

    refactor : add correct migrations

--- prisma/migrations/20240929153136_update_fields/migration.sql
+++ prisma/migrations/20240929153136_update_fields/migration.sql
@@ -15,22 +15,3 @@ ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
 -- AlterTable
 ALTER TABLE "Users" DROP COLUMN "updateAt",
 ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
-
--- CreateTable
-CREATE TABLE "Manufacturer" (
-    "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "phone" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "street" TEXT NOT NULL,
-    "number" TEXT NOT NULL,
-    "city" TEXT NOT NULL,
-    "state" TEXT NOT NULL,
-    "zip_code" TEXT NOT NULL,
-    "website" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "Manufacturer_id_key" ON "Manufacturer"("id");
