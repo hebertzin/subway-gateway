431df34de3af8409bafae83bbebc71b19c2f44d2
hebertsanto <hebertsantos0704@gmail.com>
2024-09-29 20:40:11 -0300

    feat : creating user entity

---
+++ src/domain/entities/users.ts
@@ -0,0 +1,15 @@
+export type Users = {
+  username: string;
+  password: string;
+  email: string;
+  phone: string;
+  street: string;
+  country: string;
+  nationality: string;
+  state: string;
+  city: string;
+  postal_code: string;
+  gender: string;
+  date_of_birth: string;
+  languages: string;
+};
