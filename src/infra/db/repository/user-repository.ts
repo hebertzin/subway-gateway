0e20c47c1340472e3860fb7d8e620e55febe9821
hebertsanto <hebertsantos0704@gmail.com>
2024-09-29 20:51:22 -0300

    feat : create user repository

---
+++ src/infra/db/repository/user-repository.ts
@@ -0,0 +1,14 @@
+import { User } from "../../../domain/entities/users";
+import { AddUserRepository } from "../../../domain/repositories/users/add-user-repository";
+import { prisma } from "../prisma-client";
+
+export class UserRepository implements AddUserRepository {
+  async create(userData: User): Promise<User> {
+    const user = await prisma.users.create({
+      data: {
+        ...userData,
+      },
+    });
+    return user;
+  }
+}
