55660d2a75df6f15c145bcd8cf01ca900a0fea98
hebertsanto <hebertsantos0704@gmail.com>
2024-09-29 21:07:44 -0300

    feat : adding concrete implementation to find user

--- src/infra/db/repository/user-repository.ts
+++ src/infra/db/repository/user-repository.ts
@@ -1,8 +1,9 @@
 import { User } from "../../../domain/entities/users";
 import { AddUserRepository } from "../../../domain/repository/users/add-user-repository";
+import { LoadUserByEmailRepository } from "../../../domain/repository/users/find-user-by-email";
 import { prisma } from "../prisma-client";
 
-export class UserRepository implements AddUserRepository {
+export class UserRepository implements AddUserRepository, LoadUserByEmailRepository {
   async create(userData: User): Promise<User> {
     const user = await prisma.users.create({
       data: {
@@ -10,5 +11,10 @@ export class UserRepository implements AddUserRepository {
       },
     });
     return user;
+  }
+
+  async loadByEmail(email: string): Promise<User> {
+    const user = await prisma.users.findFirst({ where: { email } });
+    return user
   }
 }
