77901cb3a068a90bc056b4dfc303e711379555c5
hebertsanto <hebertsantos0704@gmail.com>
2024-09-30 01:02:00 -0300

    refactor : rename file add manufacturer controller

--- src/infra/factory/controllers/add-manufacturer-controller-factory.ts
+++ src/infra/factory/controllers/add-manufacturer-controller-factory.ts
@@ -1,5 +1,5 @@
 import { Controller } from "../../../domain/controller";
-import { AddManufacturerController } from "../../../presentation/controllers/manufacturer";
+import { AddManufacturerController } from "../../../presentation/controllers/manufacturer/add-manufacturer-controller";
 import { makeDbAddManufacturer } from "../usecases/add-manufacturer-factory";
 
 export const makeAddManufacturerController = (): Controller => {
--- src/presentation/controllers/manufacturer/add-manufacturer-controller.ts
+++ src/presentation/controllers/manufacturer/add-manufacturer-controller.ts
@@ -1,8 +1,8 @@
 import { Request } from "express";
-import { Controller, HttpResponse } from "../../domain/controller";
-import { Manufacturer } from "../../domain/entities/manufacturer";
-import { HttpStatusCode } from "../../domain/http";
-import { AddManufacturer } from "../../domain/usecases/manufacturer/add-manufacturer-use-case";
+import { Controller, HttpResponse } from "../../../domain/controller";
+import { Manufacturer } from "../../../domain/entities/manufacturer";
+import { HttpStatusCode } from "../../../domain/http";
+import { AddManufacturer } from "../../../domain/usecases/manufacturer/add-manufacturer-use-case";
 
 export class AddManufacturerController implements Controller {
   constructor(readonly addManufacturerUseCase: AddManufacturer) {}
