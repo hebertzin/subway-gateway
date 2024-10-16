8b707ce095a2ed30182af05f82d7735c170b49f0
hebertsanto <hebertsantos0704@gmail.com>
2024-10-15 23:37:30 -0300

    feat : create add user factory

---
+++ src/infra/factory/usecases/add-user-factory.ts
@@ -0,0 +1,12 @@
+import { AddUserUseCase } from "../../../application/usecases/users/add-user-use-case";
+import { AddUser } from "../../../domains/usecases/users/add-user-use-case";
+import { UsersRepository } from "../../db/repository/user-repository";
+import { LoggerService } from "../../logging/logger";
+import { Hash } from "../../security/bcrypt/hash";
+
+export const makeDbAddUser = (): AddUser => {
+  const usersRepository = new UsersRepository();
+  const logging = new LoggerService();
+  const hash = new Hash();
+  return new AddUserUseCase(usersRepository, logging, hash);
+};
---
+++ src/infra/security/bcrypt/hash.ts
@@ -0,0 +1,14 @@
+import bcrypt from "bcrypt";
+import { Hasher } from "../../../domains/hasher";
+
+export class Hash implements Hasher {
+  private readonly saltRounds = 10;
+
+  async hash(password: string): Promise<string> {
+    return bcrypt.hash(password, this.saltRounds);
+  }
+
+  async compare(password: string, hashedPassword: string): Promise<boolean> {
+    return bcrypt.compare(password, hashedPassword);
+  }
+}
