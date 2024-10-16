0fed1fdef7c7f2425b50fb9c1f7c21169b65ee98
hebertsanto <hebertsantos0704@gmail.com>
2024-10-15 23:58:53 -0300

    refactor : ajust importations

--- src/infra/factory/usecases/manufacturer/add-manufacturer-factory.ts
+++ src/infra/factory/usecases/manufacturer/add-manufacturer-factory.ts
@@ -1,7 +1,7 @@
-import { AddManufacturerUseCase } from "../../../application/usecases/manufacturer/add-manufacturer-use-case";
-import { AddManufacturer } from "../../../domains/usecases/manufacturer/add-manufacturer-use-case";
-import { ManufacturerRepository } from "../../db/repository/manufacturer-repository";
-import { LoggerService } from "../../logging/logger";
+import { AddManufacturerUseCase } from "../../../../application/usecases/manufacturer/add-manufacturer-use-case";
+import { AddManufacturer } from "../../../../domains/usecases/manufacturer/add-manufacturer-use-case";
+import { ManufacturerRepository } from "../../../db/repository/manufacturer-repository";
+import { LoggerService } from "../../../logging/logger";
 
 export const makeDbAddManufacturer = (): AddManufacturer => {
   const manufacturerRepository = new ManufacturerRepository();
--- src/infra/factory/usecases/users/add-user-factory.ts
+++ src/infra/factory/usecases/users/add-user-factory.ts
@@ -1,8 +1,8 @@
-import { AddUserUseCase } from "../../../application/usecases/users/add-user-use-case";
-import { AddUser } from "../../../domains/usecases/users/add-user-use-case";
-import { UsersRepository } from "../../db/repository/user-repository";
-import { LoggerService } from "../../logging/logger";
-import { Hash } from "../../security/bcrypt/hash";
+import { AddUserUseCase } from "../../../../application/usecases/users/add-user-use-case";
+import { AddUser } from "../../../../domains/usecases/users/add-user-use-case";
+import { UsersRepository } from "../../../db/repository/user-repository";
+import { LoggerService } from "../../../logging/logger";
+import { Hash } from "../../../security/bcrypt/hash";
 
 export const makeDbAddUser = (): AddUser => {
   const usersRepository = new UsersRepository();
