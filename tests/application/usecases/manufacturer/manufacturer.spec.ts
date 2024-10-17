7a0adf7e6be65b15a75d09e0f5bef12bd7018ad0
hebertsanto <hebertsantos0704@gmail.com>
2024-10-17 10:24:25 -0300

    test : refactoring tests

--- tests/application/usecases/manufacturer/manufacturer.spec.ts
+++ tests/application/usecases/manufacturer/manufacturer.spec.ts
@@ -1,30 +1,24 @@
-import { describe, expect, it, vi } from "vitest";
+import { describe, expect, it } from "vitest";
 import { AppError } from "../../../../src/application/errors/errors";
 import { AddManufacturerUseCase } from "../../../../src/application/usecases/manufacturer/add-manufacturer-use-case";
 import { Manufacturer } from "../../../../src/domains/entities/manufacturer";
-import { Logging } from "../../../../src/domains/logger";
-import { AddManufacturer } from "../../../../src/domains/usecases/manufacturer/add-manufacturer-use-case";
+import { makeLoggingMock } from "../../../mocks/logging-mock";
+import { makeManufacturerRepositoryMock } from "../../../mocks/manufacture-repository-mock";
 
-const logger: Logging = {
-  error: vi.fn(),
-  info: vi.fn(),
-  warn: vi.fn(),
-  debug: vi.fn(),
-  log: vi.fn(),
-};
+const makeSut = () => {
+  const manufacturerRepositoryMock = makeManufacturerRepositoryMock();
+  const loggingMock = makeLoggingMock();
 
-const manufacturerRepositoryMock = {
-  create: vi.fn(),
-};
-
-const makeSut = (): { sut: AddManufacturer } => {
-  const sut = new AddManufacturerUseCase(manufacturerRepositoryMock, logger);
-  return { sut };
+  const sut = new AddManufacturerUseCase(
+    manufacturerRepositoryMock,
+    loggingMock
+  );
+  return { sut, manufacturerRepositoryMock, loggingMock };
 };
 
 describe("AddManufacturerUseCase", () => {
   it("should create a manufacturer successfully", async () => {
-    const { sut } = makeSut();
+    const { sut, manufacturerRepositoryMock } = makeSut();
     const input: Manufacturer = {
       name: "Fabricante XYZ",
       city: "São Paulo",
@@ -46,7 +40,7 @@ describe("AddManufacturerUseCase", () => {
   });
 
   it("should throw an error if creating manufacturer fails", async () => {
-    const { sut } = makeSut();
+    const { sut, manufacturerRepositoryMock} = makeSut();
     const input: Manufacturer = {
       name: "Fabricante XYZ",
       city: "São Paulo",
